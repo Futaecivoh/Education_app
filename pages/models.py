61ad165d80a57fda656fe9510207e2b36ee1775c
Futaecivoh <68806585+Futaecivoh@users.noreply.github.com>
2026-05-22 23:59:09 +0300

    Task 5 ready postgreSQL and modelling

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Course(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Название курса")
+    description = models.TextField(verbose_name="Описание")
+    price = models.IntegerField(verbose_name="Цена (в рублях)")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+
+    def __str__(self):
+        return f"{self.title} ({self.price} руб.)"
+        
+    class Meta:
+        verbose_name = "Курс"
+        verbose_name_plural = "Курсы"
