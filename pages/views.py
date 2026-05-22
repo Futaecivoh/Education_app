56666d324bc773eb73dd0bd363a30545e1208bcf
Futaecivoh <68806585+Futaecivoh@users.noreply.github.com>
2026-05-22 23:09:02 +0300

    Task 4 ready, inheritance and static

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'pages/index.html')
+    context = {
+        'title': 'Школа японского языка SAKURANBO',
+        'welcome_text': 'Ваше путешествие в мир Японского начинается здесь',
+        'lead_text': 'Мы предлагаем самые уютные и качественные курсы японского языка. Здесь вы не просто выучите язык с нуля, но и найдете множество новых друзей.'
+    }
+    return render(request, 'pages/index.html', context)
+
+def materials(request):
+    context = {
+        'title': 'Наши материалы',
+    }
+    return render(request, 'pages/materials.html', context)
